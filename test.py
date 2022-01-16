f6bb25da27ac22d3ae9b85bdab24dd51325289bf
nandhinidevi1703 <97379672+nandhinidevi1703@users.noreply.github.com>
2022-01-16 15:08:05 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,37 @@
+a="10"
+
+print(a)
+print(type(a))
+
+print("string to int conversion")
+b=int(a)
+
+print(b)
+print(type(b))
+
+print("string to float conversion")
+c=float(a)    
+
+print(c)
+print(type(c))
+
+print("string to bool conversion")
+d=bool(a)    
+
+print(d)
+print(type(d))
+
+'''
+output:
+10
+<class 'str'>
+string to int conversion
+10
+<class 'int'>
+string to float conversion
+10.0
+<class 'float'>
+string to bool conversion
+True
+<class 'bool'>
+'''
